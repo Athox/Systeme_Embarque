aa7888b611768d8d1c38869e8768e158830f5764
Athox <cot.anthony@gmail.com>
2016-12-05 17:50:18 +0100

    début buttons

--- app/src/main/java/com/example/acha_ou_teung/systeme_embarque/MainActivity.java
+++ app/src/main/java/com/example/acha_ou_teung/systeme_embarque/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.acha_ou_teung.systeme_embarque;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button_inscription, button_connexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonInscription = (Button)findViewById(R.id.button_inscription);
     }
 }
