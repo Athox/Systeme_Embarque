170e2896158dd74385b954b72edd69eedc8bd4c0
Athox <cot.anthony@gmail.com>
2016-12-05 17:26:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/acha_ou_teung/systeme_embarque/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.acha_ou_teung.systeme_embarque;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
